5bd738aa814ffe8de5cd66b76b8bd1e2f7740653
Ravindra R. Jaju <jaju@msync.org>
2013-01-12 22:22:48 +0530

    Lein profiles.clj - initial.

---
+++ rc/lein-profiles.clj
@@ -0,0 +1,8 @@
+{:user {:plugins [[lein-immutant "0.14.1"]
+                  [org.clojure/tools.nrepl "0.2.0-RC1"]
+                  [lein-exec "0.2.1"]
+                  [lein-ring "0.8.0-SNAPSHOT"]
+                  [lein-localrepo "0.4.1"]
+                  [lein-ritz "0.6.0"]]
+        :dependencies [[clj-ns-browser "1.3.0"]]}
+ }
