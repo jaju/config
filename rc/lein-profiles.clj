7646c1ac01886abf043af2767da6adcff8f9d2df
Ravindra R. Jaju <jaju@msync.org>
2013-04-18 05:35:23 +0530

    Updated lein profiles file.

--- rc/lein-profiles.clj
+++ rc/lein-profiles.clj
@@ -1,8 +1,9 @@
-{:user {:plugins [[lein-immutant "0.14.1"]
-                  [org.clojure/tools.nrepl "0.2.0-RC1"]
+{:user {:plugins [[lein-immutant "0.17.1"]
+                  [org.clojure/tools.nrepl "0.2.0"]
                   [lein-exec "0.2.1"]
-                  [lein-ring "0.8.0-SNAPSHOT"]
-                  [lein-localrepo "0.4.1"]
-                  [lein-ritz "0.6.0"]]
-        :dependencies [[clj-ns-browser "1.3.0"]]}
+                  [lein-ring "0.8.0"]
+                  [lein-localrepo "0.4.1"]]
+        :dependencies [[clj-ns-browser "1.3.0"]
+                       [slamhound "1.3.3"]]
+        }
  }
