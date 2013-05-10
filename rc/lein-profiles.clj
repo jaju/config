0c81ac8b682e7f67b4c4ea9540a97defdf2a5b86
Ravindra R. Jaju <jaju@msync.org>
2013-05-10 18:00:08 +0530

    Updated leiningen plugins to current latest stable ones.

--- rc/lein-profiles.clj
+++ rc/lein-profiles.clj
@@ -1,9 +1,11 @@
-{:user {:plugins [[lein-immutant "0.17.1"]
-                  [org.clojure/tools.nrepl "0.2.0"]
-                  [lein-exec "0.2.1"]
-                  [lein-ring "0.8.0"]
-                  [lein-localrepo "0.4.1"]]
-        :dependencies [[clj-ns-browser "1.3.0"]
-                       [slamhound "1.3.3"]]
+{:user {:plugins [[lein-immutant "0.18.1"]
+                  [org.clojure/tools.nrepl "0.2.2"]
+                  [lein-exec "0.3.0"]
+                  [lein-ring "0.8.5"]
+                  [lein-localrepo "0.4.1"]
+                  [lein-cljsbuild "0.3.0"]]
+        :dependencies
+                [[clj-ns-browser "1.3.1"]
+                 [slamhound "1.3.3"]]
         }
  }
